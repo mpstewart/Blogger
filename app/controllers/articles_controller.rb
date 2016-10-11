d4e4b94221f32b8d1b88620c3869b1f18a0482c8
Michael Stewart <mps1991@gmail.com>
2016-10-10 21:32:53 -0400

    Added record deletion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 
 	def index
 		@articles = Article.all
@@ -7,8 +8,24 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-
+		
+	# create a new record instance to manipulate
 	def new
+		@article = Article.new
+	end
+	
+	# receive the form information and make the new record
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		redirect_to article_path(@article)
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,6 @@
 module ArticlesHelper
+	# stores params
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
 end
