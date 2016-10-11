e46feab44507b49fba49f8644f5034cd23b34b8b
Michael Stewart <mps1991@gmail.com>
2016-10-11 19:35:59 -0400

    Added comment deletion functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,5 +9,15 @@ class CommentsController < ApplicationController
 
 		redirect_to article_path(@comment.article)
 	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+
+		@comment = @article.comments.find(params[:id])
+
+		@comment.destroy
+
+		redirect_to article_path(@article)
+	end
 end
 
