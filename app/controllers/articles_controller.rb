79120dda9e458578b76f7fa7a3760101759c407c
Michael Stewart <mps1991@gmail.com>
2016-10-11 14:50:14 -0400

    Completed section 11
    Completed section 11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,52 @@
 class ArticlesController < ApplicationController
 	include ArticlesHelper
 
+	# show all records
 	def index
 		@articles = Article.all
 	end
 
+	# show a particular record
 	def show
 		@article = Article.find(params[:id])
 	end
 		
 	# create a new record instance to manipulate
+	# show the new.html.erb template
 	def new
 		@article = Article.new
 	end
 	
 	# receive the form information and make the new record
+	# article_params is defined in articles_helper.rb
 	def create
 		@article = Article.new(article_params)
 		@article.save
 
+		flash.notice = "Article '#{@article.title}' Created!"
+
 		redirect_to article_path(@article)
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	# same path as show, the difference is the HTTP method used
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
 
+		flash.notice = "Article '#{@article.title}' Deleted!"
 		redirect_to articles_path
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title} Updated!"
+
+		redirect_to article_path(@article)
 	end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,8 @@
 module ArticlesHelper
-	# stores params
+	# verifies params coming in.
+	# since this is how the application receives data to put into the model
+	# it would be a bad idea to just take that information at face value.
+	# this kind of checking is required by rails, and using a helper method is an easy way to do it
 	def article_params
 		params.require(:article).permit(:title, :body)
 	end
