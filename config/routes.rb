9978bd3782d6a3111894f8a368c7d9e07bd2e9d8
Michael Stewart <mike@MBPr.local>
2016-10-16 19:43:56 -0400

    Added tagging system. Users are able to add and browse by tags on articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+	# the root url will now call articles.index
+	root to:'articles#index'
+
 	resources :articles do
 		resources :comments
 	end
+	resources :tags
 
-	# the root url will now call articles.index
-	root to:'articles#index'
 end
