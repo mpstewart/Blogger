91d2955f6150bf609717dadc1b0bd6948eab3198
Michael Stewart <mps1991@gmail.com>
2016-10-21 15:15:41 -0400

    Added image uploading and added home button to layout

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,6 +4,6 @@ module ArticlesHelper
 	# it would be a bad idea to just take that information at face value.
 	# this kind of checking is required by rails, and using a helper method is an easy way to do it
 	def article_params
-		params.require(:article).permit(:title, :body, :tag_list)
+		params.require(:article).permit(:title, :body, :tag_list, :image)
 	end
 end
