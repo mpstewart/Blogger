a599bd3fb3597437416cf699a2b6b2b24a2deef8
Michael Stewart <mps1991@gmail.com>
2016-10-22 00:16:40 -0400

    removed extra migrations

--- db/migrate/20161022032816_add_author_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToArticle < ActiveRecord::Migration[5.0]
-  def change
-    add_column :articles, :author, :reference
-  end
-end
--- db/migrate/20161022033041_add_articles_to_author.rb
+++
@@ -1,5 +0,0 @@
-class AddArticlesToAuthor < ActiveRecord::Migration[5.0]
-  def change
-    add_column :authors, :articles, :reference
-  end
-end
